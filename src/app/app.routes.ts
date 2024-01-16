d47d364608dc73bbbd7475298f121ea3e093d996
kaleck hamm <kaleckh@gmail.com>
2024-01-15 20:06:15 -0700

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {Home} from './pages/Home/home.component';
+import {About} from './pages/About/about.component';
+import {Test} from './pages/Test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: Home},    
+    {path: "about", component: About},    
+    {path: "test", component: Test},    
+];
---
+++ src/app/pages/Home/home.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'home',
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class Home implements OnInit {
+  title = 'Home Page';
+  pokemon: any;
+  getPokemon: any;
+  
+  ngOnInit(): void {
+    this.getPokemon();
+  }
+
+//   constructor() {
+//     this.pokemon = ''
+//     this.getPokemon = async () => {
+//       try {
+//         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/infernape`);
+//         const url = await res.json()
+//         if (typeof url.sprites.other.home.front_default === 'string') this.pokemon = url.sprites.other.home.front_default;
+//         return;
+//       } catch (err) {
+//         return 'error';
+//       }
+//     };
+//   }
+}
